67b34380abc62cfd42d466158db97d0135a66180
jongminh <jaisonhong@gmail.com>
2018-07-03 00:45:54 -0400

    Completed mongoDB section 4

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 const Post = require('./models/post');
 
 const app = express();
@@ -34,30 +33,31 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully!'
-  })
+  post.save().then( result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Post added successfully!',
+      postId: result._id
+    })
+  });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fasdas1231',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'asdasd2131',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'
-    }
-  ];
-  return res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then( documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id})
+    .then( result => {
+      console.log(result);
+      res.status(200).json({message:'Post deleted!'});
+    });
+})
 
 module.exports = app;
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -32,6 +32,11 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = posts;
       });
   }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
   ngOnDestroy() {
     this.postSub.unsubscribe();
   }
