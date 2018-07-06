c84a21f608f8feea192327b21a80c5d7ebdfa6c9
jongminh <jaisonhong@gmail.com>
2018-07-05 21:16:28 -0400

    Done File update Section 6

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -28,15 +28,22 @@ const storage = multer.diskStorage({
 });
 
 router.post('', multer({storage: storage}).single("image"), (req, res, next) => {
+  const url = req.protocol + "://"  + req.get("host");
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + "/images/" + req.file.filename
   });
-  post.save().then( result => {
-    console.log(result);
+  post.save().then( createdPost => {
     res.status(201).json({
       message: 'Post added successfully!',
-      postId: result._id
+      post: {
+        ...createdPost,
+        id: createdPost._id,
+        // title: createdPost.title,
+        // content: createdPost.content,
+        // imagePath: createdPost.imagePath
+      }
     })
   });
 });
@@ -64,11 +71,18 @@ router.get("/:id", (req, res, next) =>{
     });
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id',
+  multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + "://"  + req.get("host");
+    imagePath = url + "/images/" + req.file.filename
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
   Post.updateOne({_id: req.params.id}, post)
     .then( result => {
